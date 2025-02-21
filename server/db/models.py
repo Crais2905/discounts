87569eabaf9a71463644de26e08e16f464524d90
Crais2905 <slisovets2905@gmail.com>
2025-02-21 15:04:10 +0200

    add tables store and discount

--- server/db/models.py
+++ server/db/models.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Mapped, declarative_base, mapped_column, relationship
-from sqlalchemy import String, ForeignKey, Integer
+from sqlalchemy import String, ForeignKey, Integer, DateTime
 from typing import Optional
 
 Base = declarative_base()
@@ -29,8 +29,21 @@ class Category(Base):
 
 
 class Discount(Base):
-    pass
+    __tablename__ = 'discount'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    old_price: Mapped[int] = mapped_column(nullable=False)  
+    new_price: Mapped[int] = mapped_column(nullable=False)  
+    discount_percent: Mapped[int] = mapped_column(nullable=False)
+    start_date: Mapped[DateTime] = mapped_column(nullable=True)
+    end_date: Mapped[DateTime] = mapped_column(nullable=False)
+    product_id: Mapped[int] = mapped_column(Integer, ForeignKey("product.id"))
+    product: Mapped[Product] = relationship(Product, back_populates='discount', lazy='selectin')
 
 
 class Store(Base):
-    pass
+    __tablename__ = 'store'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
