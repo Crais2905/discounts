4ff63544965810086b4e500d6a122ce5b31f28c6
Crais2905 <slisovets2905@gmail.com>
2025-02-22 23:47:08 +0200

    add routers for category

--- server/api/category/category.py
+++ server/api/category/category.py
@@ -1,14 +1,57 @@
-from fastapi import APIRouter, Depends
-# from schemas.category import CategoryCreate, CategoryUpdate, CategoryPublic
+from fastapi import APIRouter, Depends, HTTPException, status
+from schemas.category import CategoryCreate, CategoryUpdate, CategoryPublic
 from ..services.category_crud import CategoryCrud
 
 router = APIRouter()
 
 
-@router.get("/")
+@router.post('/', response_model=CategoryPublic, status_code=status.HTTP_201_CREATED)
+async def create_category(
+    category_data: CategoryCreate,
+    category_crud: CategoryCrud = Depends(CategoryCrud)
+):
+    return await category_crud.create_category(category_data)
+
+
+@router.get("/", response_model=list[CategoryPublic])
 async def get_categories(
     offset: int = 0,
     limit: int = 10,
     category_crud: CategoryCrud = Depends(CategoryCrud)
 ):
     return await category_crud.get_categories(offset, limit)
+
+
+@router.get("/{category_id}", response_model=CategoryPublic)
+async def get_category(
+    category_id: int,
+    category_crud: CategoryCrud = Depends(CategoryCrud)
+):
+    category = await category_crud.get_category(category_id)
+    if not category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+    return category
+
+
+@router.put("/{category_id}", response_model=CategoryPublic)
+async def full_update_category(
+    category_id: int,
+    category_data: CategoryCreate,
+    category_crud: CategoryCrud = Depends(CategoryCrud)
+):
+    category = await category_crud.get_category(category_id)
+    if not category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+    return  await category_crud.update_category(category_data, category_id)
+
+
+@router.delete("/{category_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_category(
+    category_id: int,
+    category_crud: CategoryCrud = Depends(CategoryCrud)
+):
+    category = await category_crud.get_category(category_id)
+    if not category:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Category not found")
+    await category_crud.delete_category(category_id)
+    return
--- server/api/services/category_crud.py
+++ server/api/services/category_crud.py
@@ -3,17 +3,55 @@ from sqlalchemy.future import select
 from sqlalchemy import update, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from db.session import get_session
-from db.models import Category
-
+from db.models import Category, Product
+from schemas.category import CategoryCreate, CategoryUpdate
 
 class CategoryCrud:
     def __init__(self, session: AsyncSession = Depends(get_session)):
         self.session = session
 
     
+    async def create_category(self, category_data: CategoryCreate):
+        stmt = insert(Category).values(category_data.model_dump()).returning(Category)
+        result = await self.session.execute(stmt)
+        await self.session.commit()
+        inserted_category = result.scalar()
+        return inserted_category
+
+
     async def get_categories(self, offset: int, limit: int):
         stmt = select(Category).offset(offset).limit(limit)
         return await self.session.scalars(stmt)
     
+
+    async def get_category(self, category_id):
+        stmt = select(Category).where(Category.id == category_id)
+        return await self.session.scalar(stmt)
+    
+
+    async def update_category(self, category_data: CategoryUpdate, category_id: int):
+        values = category_data.model_dump(exclude_unset=True)
+        stmt = update(Category).where(Category.id == category_id).values(values).returning(Category)
+        result = await self.session.execute(stmt)
+        await self.session.commit()
+        inserted_category = result.scalar()
+        return inserted_category
+    
+
+    async def delete_category(self, category_id: int):
+        stmt = delete(Category).where(Category.id == category_id).returning(Category)
+        stmt_products = delete(Product).where(Product.category_id == category_id).returning(Product)
+
+        async with self.session as conn:
+            try:
+                stmt_result = await conn.execute(stmt)
+                stmt_products_result = await conn.execute(stmt_products)
+                await conn.commit()
+            except Exception as error:
+                await conn.rollback()
+                raise error
+
+        result = [stmt_result.scalar(), stmt_products_result.scalars().all()]
+        return result
 
     
