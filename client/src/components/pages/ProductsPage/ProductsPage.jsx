be2369163a89b13b309f88454b72d524faa47681
Crais2905 <slisovets2905@gmail.com>
2025-06-16 22:52:27 +0300

    add dockerfiles for client and server

--- client/src/components/pages/ProductsPage/ProductsPage.jsx
+++ client/src/components/pages/ProductsPage/ProductsPage.jsx
@@ -1,4 +1,3 @@
-import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 
@@ -9,16 +8,16 @@ export default function ProductPage() {
     const [products, setProducts] = useState([])
 
     const fetchProducts = () => {
-            axios.get('http://127.0.0.1:8000/products/?offset=0&limit=10&sorted_by_date=true').then(r => {
-            const response = r.data;
-            setProducts(response)
-            });
-        };
+        axios.get('http://127.0.0.1:8000/products/?offset=0&limit=10&sorted_by_date=true').then(r => {
+        const response = r.data;
+        setProducts(response)
+        });
+    };
 
-     useEffect(() => {
-            fetchProducts();
-        }, []);
-    
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
     if (!products) return <p>Loading...</p>;
 
     return (
